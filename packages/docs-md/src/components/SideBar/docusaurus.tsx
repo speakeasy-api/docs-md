a24d0147cde8d686f152adf4a017744884189b00
nebrius <bryan.hughes@speakeasy.com>
2025-07-07 13:15:49 -0700

    Normalized use client directives

--- packages/docs-md/src/components/SideBar/docusaurus.tsx
+++ packages/docs-md/src/components/SideBar/docusaurus.tsx
@@ -1,9 +1,3 @@
-// IMPORTANT! This file MUST NOT be marked as "use client", otherwise it will
-// cause Nextra to error when trying to render. This is because MDX files cannot
-// import files marked with "use client", for some reason, but it's perfectly
-// happy to import a server component (this file) that then imports a client
-// component.
-
 import type { SideBarTriggerProps } from "./common/containers.tsx";
 import {
   SideBarContents,
