79faa76c31be099c44cabc5bb1eacbe023b1b8b7
nebrius <bryan.hughes@speakeasy.com>
2025-07-10 16:29:11 -0700

    Nextra pill styles

--- packages/docs-md/src/components/Pill/nextra/Pill.tsx
+++ packages/docs-md/src/components/Pill/nextra/Pill.tsx
@@ -1,5 +1,8 @@
+import clsx from "clsx";
+
 import type { PillProps } from "../common/types.ts";
+import styles from "./styles.module.css";
 
 export function NextraPill({ variant, children }: PillProps) {
-  return <span className={`pill pill-${variant}`}>{children}</span>;
+  return <span className={clsx(styles.pill, styles[variant])}>{children}</span>;
 }
