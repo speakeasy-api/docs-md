75c1ae1923bbbea09433e82cb761a175cf9c2c0d
nebrius <bryan.hughes@speakeasy.com>
2025-07-16 13:40:49 -0700

    Brought in minimal Mistral example

--- examples/docusaurus/mistral/src/components/HomepageFeatures/index.tsx
+++ examples/docusaurus/mistral/src/components/HomepageFeatures/index.tsx
@@ -5,7 +5,7 @@ export default function HomepageFeatures(): ReactNode {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">We have some cool features</div>
+        <div className="row">Mistral has some cool features</div>
       </div>
     </section>
   );
