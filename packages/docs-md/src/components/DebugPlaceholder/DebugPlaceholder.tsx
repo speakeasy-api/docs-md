f4dfb89b6ab3694738ac2b71be2f15a0a3248ac7
nebrius <bryan.hughes@speakeasy.com>
2025-07-23 17:17:27 +0100

    Import fix

--- packages/docs-md/src/components/DebugPlaceholder/DebugPlaceholder.tsx
+++ packages/docs-md/src/components/DebugPlaceholder/DebugPlaceholder.tsx
@@ -1,6 +1,6 @@
 import type { PropsWithChildren } from "react";
 
-import styles from "./common/styles.module.css";
+import styles from "./styles.module.css";
 
 export function DebugPlaceholder({ children }: PropsWithChildren) {
   return <div className={styles.debugPlacholder}>{children}</div>;
