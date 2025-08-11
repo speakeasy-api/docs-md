beb37813f945b84e54d892034d1c991e5e865529
nebrius <bryan.hughes@speakeasy.com>
2025-08-11 16:56:45 -0700

    Scaffolded expandable sections

--- packages/docs-md/src/compiler/renderers/base/webComponents.ts
+++ packages/docs-md/src/compiler/renderers/base/webComponents.ts
@@ -1,4 +1,7 @@
+import { HEADINGS } from "../../content/constants.ts";
 import type {
+  RendererAddExpandableBreakoutArgs,
+  RendererAddExpandablePropertyArgs,
   RendererCreatePillArgs,
   RendererCreateSectionArgs,
   RendererCreateSectionContentArgs,
@@ -86,6 +89,89 @@ export abstract class WebComponentRenderer extends MarkdownRenderer {
 
   public override getIdSeparator() {
     return "_";
+  }
+
+  public override addExpandableBreakout(
+    ...[{ createTitle, createContent }]: RendererAddExpandableBreakoutArgs
+  ) {
+    const { id, parentId } = this.#getBreakoutIdInfo();
+    this.appendText(
+      `<speakeasy-expandable-breakout
+    slot="entry"
+    id="${id}"
+    headingId="${this.getCurrentId()}"${parentId ? ` parentId="${parentId}"` : ""}
+    hasFrontMatter={${createContent ? "true" : "false"}}
+  >`,
+      { escape: "none" }
+    );
+
+    this.appendText(`<div slot="title">`);
+    createTitle();
+    this.appendText("</div>");
+
+    if (createContent) {
+      this.appendText(`<div slot="content">`);
+      createContent();
+      this.appendText("</div>");
+    }
+
+    this.appendText("</speakeasy-expandable-breakout>");
+  }
+
+  public override addExpandableProperty(
+    ...[
+      { typeInfo, annotations, title, createContent },
+    ]: RendererAddExpandablePropertyArgs
+  ) {
+    const { id, parentId } = this.#getBreakoutIdInfo();
+    this.appendText(
+      `<speakeasy-expandable-property slot="entry" id="${id}" headingId="${this.getCurrentId()}"${
+        parentId ? ` parentId="${parentId}"` : ""
+      }${
+        typeInfo
+          ? ` typeInfo="${JSON.stringify(typeInfo).replaceAll('"', "&quot;")}"`
+          : ""
+      }${
+        annotations.length > 0
+          ? ` typeAnnotations="${JSON.stringify(annotations).replaceAll('"', "&quot;")}"`
+          : ""
+      } hasFrontMatter="${createContent ? "true" : "false"}">`,
+      { escape: "none" }
+    );
+
+    this.appendText(`<div slot="title">`);
+    this.appendHeading(HEADINGS.PROPERTY_HEADING_LEVEL, title, {
+      escape: "mdx",
+      id: this.getCurrentId(),
+    });
+    this.appendText("</div>");
+
+    if (createContent) {
+      this.appendText(`<div slot="content">`);
+      createContent();
+      this.appendText("</div>");
+    }
+
+    this.appendText(`</speakeasy-expandable-property>`);
+  }
+
+  #getBreakoutIdInfo() {
+    const stack = this.getContextStack().map((c) => c.id);
+    const id = stack.join("_");
+    const parentId = stack.slice(0, -1).join("_") || undefined;
+    return { id, parentId };
+  }
+
+  protected override handleCreateSecurity(cb: () => void) {
+    this.appendText("<speakeasy-expandable-section>");
+    cb();
+    this.appendText("</speakeasy-expandable-section>");
+  }
+
+  protected override handleCreateParameters(cb: () => void) {
+    this.appendText("<speakeasy-expandable-section>");
+    cb();
+    this.appendText("</speakeasy-expandable-section>");
   }
 
   protected override handleCreateBreakouts(cb: () => void) {
