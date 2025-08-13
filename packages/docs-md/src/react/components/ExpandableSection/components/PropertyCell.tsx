e00d5cdda5f182f5bf8bcc94b824eb00152624c0
nebrius <bryan.hughes@speakeasy.com>
2025-08-13 15:23:29 -0700

    Rebuilt components

--- packages/docs-md/src/react/components/ExpandableSection/components/PropertyCell.tsx
+++ packages/docs-md/src/react/components/ExpandableSection/components/PropertyCell.tsx
@@ -13,8 +13,8 @@ import {
   computeMultilineTypeLabel,
   computeSingleLineDisplayType,
 } from "../../../util/displayType.ts";
-import { Pill } from "../../Pill/Pill.tsx";
 import { useChildren, useUniqueChild } from "../../../util/hooks.ts";
+import { Pill } from "../../Pill/Pill.tsx";
 import styles from "../styles.module.css";
 
 type PropertyCellProps = PropsWithChildren<{
