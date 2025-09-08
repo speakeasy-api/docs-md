6797314a2e5c83abdabcfb1623d97f12a8f8ae50
nebrius <bryan.hughes@speakeasy.com>
2025-09-08 11:35:27 -0700

    Deleted dead code

--- packages/docs-md/src/types/shared.ts
+++ packages/docs-md/src/types/shared.ts
@@ -22,25 +22,6 @@ export type PropertyAnnotations = {
   variant: PillVariant;
 };
 
-export type TryItNowProps = {
-  /**
-   * These are dependencies that are required by the code snippet,
-   * like "zod" or an npm package.
-   */
-  externalDependencies?: Record<string, string>;
-  /**
-   * Starting value of the editor
-   */
-  defaultValue?: string;
-  /**
-   * Experimental: When enabled, the editor will automatically
-   * scan for external dependencies from npm as the user adds them
-   * as imports.
-   */
-  _enableUnsafeAutoImport?: boolean;
-  layoutStyle?: React.CSSProperties;
-};
-
 export type PageMetadataSection = {
   fragment: string;
   properties: {
