4ff3484e0c2208bd407b31bc7fcb4428971343f5
Jesus Garnica <jesus@speakeasy.com>
2025-09-24 09:22:46 -0700

    feat: add monaco editor as editor for tryitnow

--- packages/react/src/components/TryItNow/components/Editor.tsx
+++ packages/react/src/components/TryItNow/components/Editor.tsx
@@ -1,12 +1,41 @@
 "use client";
 
+import { useState, useCallback } from "react";
 import type { EditorProps } from "../types";
+import type { editor } from "monaco-editor";
+import MonacoEditor, { Monaco } from "@monaco-editor/react";
+import styles from "./styles.module.css";
 
 export function Editor({ defaultValue, onValueChange }: EditorProps) {
-  console.log(onValueChange);
+  const [_, setIsEditorReady] = useState(false);
+
+  function handleEditorDidMount(_: editor.IStandaloneCodeEditor, _2: Monaco) {
+    setIsEditorReady(true);
+  }
+
+  const handleValueChange = useCallback(
+    (value: string | undefined, _: editor.IModelContentChangedEvent) => {
+      onValueChange(value ?? "");
+    },
+    [onValueChange]
+  );
+
   return (
-    <pre>
-      <code>{defaultValue}</code>
-    </pre>
+    <MonacoEditor
+      height={"100%"}
+      loading=""
+      wrapperProps={{
+        className: styles.editorWrapper,
+      }}
+      options={{
+        minimap: {
+          enabled: false,
+        },
+      }}
+      language="typescript"
+      value={defaultValue}
+      onChange={handleValueChange}
+      onMount={handleEditorDidMount}
+    />
   );
 }
