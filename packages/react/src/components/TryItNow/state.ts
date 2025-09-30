fd0040e0c5d68bbd717ac6994b9bd359e3f32f35
nebrius <bryan.hughes@speakeasy.com>
2025-09-30 13:58:54 -0700

    Wired up super basic version of events to UI

--- packages/react/src/components/TryItNow/state.ts
+++ packages/react/src/components/TryItNow/state.ts
@@ -1,3 +1,4 @@
+import type { RuntimeEvents } from "@speakeasy-api/docs-md-shared";
 import { Runtime } from "@speakeasy-api/docs-md-shared";
 import { useCallback, useRef, useState } from "react";
 
@@ -10,9 +11,11 @@ type Options = {
 };
 
 export function useRuntime({ packageManagerUrl, dependencies }: Options) {
-  const [status] = useState<Status>({
+  const [status, setStatus] = useState<Status>({
     state: "idle",
   });
+  const previousEvents = useRef<RuntimeEvents[]>([]);
+  const events = useRef<RuntimeEvents[]>([]);
   const runtimeRef = useRef<Runtime | null>(null);
 
   if (!runtimeRef.current) {
@@ -20,26 +23,63 @@ export function useRuntime({ packageManagerUrl, dependencies }: Options) {
       packageManagerUrl,
       dependencies,
     });
-    runtimeRef.current.on("compilation:started", (event) => {
-      console.log("compilation:started", event);
+    runtimeRef.current.on("compilation:started", () => {
+      previousEvents.current = events.current;
+      // We don't store started and finished events to keep event history clean
+      // for the UI. They can be inferred from the state, and don't contain
+      // any useful information for the UI.
+      events.current = [];
+      setStatus({
+        state: "compiling",
+        previousEvents: previousEvents.current,
+      });
     });
-    runtimeRef.current.on("compilation:finished", (event) => {
-      console.log("compilation:finished", event);
+    runtimeRef.current.on("compilation:finished", () => {
+      setStatus({
+        state: "executing",
+        events: events.current,
+      });
     });
     runtimeRef.current.on("compilation:error", (event) => {
-      console.log("compilation:error", event);
+      // Save previous events to events, so that next time we try to run code,
+      // they will again become previous events. This way, we always show the
+      // last _successfully_ compiled events
+      events.current = previousEvents.current;
+      setStatus({
+        state: "compile-error",
+        previousEvents: previousEvents.current,
+        // We still want to send the compilation error event thoughs, so that
+        // the UI can show a compilation error.
+        events: [event],
+      });
     });
     runtimeRef.current.on("execution:started", (event) => {
-      console.log("execution:started", event);
+      events.current.push(event);
+      setStatus({
+        state: "executing",
+        events: events.current,
+      });
     });
     runtimeRef.current.on("execution:log", (event) => {
-      console.log("execution:log", event);
+      events.current.push(event);
+      setStatus({
+        state: "executing",
+        events: events.current,
+      });
     });
     runtimeRef.current.on("execution:uncaught-exception", (event) => {
-      console.log("execution:uncaught-exception", event);
+      events.current.push(event);
+      setStatus({
+        state: "executing",
+        events: events.current,
+      });
     });
     runtimeRef.current.on("execution:uncaught-rejection", (event) => {
-      console.log("execution:uncaught-rejection", event);
+      events.current.push(event);
+      setStatus({
+        state: "executing",
+        events: events.current,
+      });
     });
   }
 
