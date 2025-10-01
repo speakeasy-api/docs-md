dac1b5341554ff2561ac7686e761899f6f67ea21
nebrius <bryan.hughes@speakeasy.com>
2025-10-01 12:59:59 -0700

    Cleanup

--- packages/compiler/src/data/generateCodeSamples.ts
+++ packages/compiler/src/data/generateCodeSamples.ts
@@ -5,7 +5,7 @@ import type { Chunk, OperationChunk } from "@speakeasy-api/docs-md-shared";
 import { info } from "../logging.ts";
 import { getSettings } from "../settings.ts";
 
-type CodeSnippet = {
+type CodeSample = {
   operationId: string;
   language: string;
   code: string;
@@ -16,7 +16,7 @@ type CodeSnippet = {
 };
 
 type CodeSamplesResponse = {
-  snippets: Omit<CodeSnippet, "packageName">[];
+  snippets: Omit<CodeSample, "packageName">[];
 };
 
 type ErrorResponse = {
@@ -24,26 +24,26 @@ type ErrorResponse = {
   statusCode: number;
 };
 
-const CODE_SNIPPETS_API_URL =
-  process.env.SPEAKEASY_CODE_SNIPPETS_API_URL ?? "https://api.speakeasy.com";
+const CODE_SAMPLES_API_URL =
+  process.env.SPEAKEASY_CODE_SAMPLES_API_URL ?? "https://api.speakeasy.com";
 
-// Map from operation ID to language to code snippet
-export type DocsCodeSnippets = Record<
+// Map from operation ID to language to code sample
+export type DocsCodeSamples = Record<
   OperationChunk["id"],
-  Record<string, CodeSnippet>
+  Record<string, CodeSample>
 >;
 
-export async function generateCodeSnippets(
+export async function generateCodeSamples(
   docsData: Map<string, Chunk>,
   specContents: string
-): Promise<DocsCodeSnippets> {
+): Promise<DocsCodeSamples> {
   const { spec, codeSamples } = getSettings();
   if (!codeSamples) {
-    info("Code samples not enabled, skipping code snippets generation");
+    info("Code samples not enabled, skipping code samples generation");
     return {};
   }
 
-  const docsCodeSnippets: DocsCodeSnippets = {};
+  const docsCodeSamples: DocsCodeSamples = {};
   const specFilename = spec && basename(spec);
 
   // create a by operationId map of the operation chunks
@@ -54,8 +54,8 @@ export async function generateCodeSnippets(
     }
   }
 
-  // Fetch code snippets from the preview api
-  const codeSampleResponses = new Map<string, CodeSnippet[]>();
+  // Fetch code samples from the preview api
+  const codeSampleResponses = new Map<string, CodeSample[]>();
   for (const language of codeSamples) {
     const formData = new FormData();
 
@@ -65,7 +65,7 @@ export async function generateCodeSnippets(
     formData.append("package_name", language.packageName);
     formData.append("sdk_class_name", language.sdkClassName);
 
-    const res = await fetch(`${CODE_SNIPPETS_API_URL}/v1/code_sample/preview`, {
+    const res = await fetch(`${CODE_SAMPLES_API_URL}/v1/code_sample/preview`, {
       method: "POST",
       body: formData,
     });
@@ -76,23 +76,23 @@ export async function generateCodeSnippets(
       const error = json as ErrorResponse;
       throw new Error(`Failed to generate code sample: ${error.message}`);
     }
-    const codeSnippets = (json as CodeSamplesResponse).snippets.map(
-      (snippet) => ({
-        ...snippet,
+    const codeSamples = (json as CodeSamplesResponse).snippets.map(
+      (sample) => ({
+        ...sample,
         packageName: language.packageName,
       })
     );
-    codeSampleResponses.set(language.language, codeSnippets);
+    codeSampleResponses.set(language.language, codeSamples);
   }
 
-  // Populate docsCodeSnippets with the code samples, prioritizing code samples
+  // Populate docsCodeSamples with the code samples, prioritizing code samples
   // from the OAS and falling back to the fetched samples if absent
   for (const chunk of docsData.values()) {
     if (chunk.chunkType !== "operation") {
       continue;
     }
 
-    const chunkCodeSnippets: Record<string, CodeSnippet> = {};
+    const chunkCodeSamples: Record<string, CodeSample> = {};
 
     // First, populate the list of samples fetched remotely
     for (const [language, codeSampleResponse] of codeSampleResponses) {
@@ -101,7 +101,7 @@ export async function generateCodeSnippets(
       );
       if (operationCodeSampleResponses.length > 0) {
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        chunkCodeSnippets[language] = operationCodeSampleResponses[0]!;
+        chunkCodeSamples[language] = operationCodeSampleResponses[0]!;
       }
     }
 
@@ -109,7 +109,7 @@ export async function generateCodeSnippets(
     for (const [language, code] of Object.entries(
       chunk.chunkData.codeSamples
     )) {
-      chunkCodeSnippets[language] = {
+      chunkCodeSamples[language] = {
         code,
         language,
         operationId: chunk.chunkData.operationId,
@@ -119,8 +119,8 @@ export async function generateCodeSnippets(
         packageName: "",
       };
     }
-    docsCodeSnippets[chunk.id] = chunkCodeSnippets;
+    docsCodeSamples[chunk.id] = chunkCodeSamples;
   }
 
-  return docsCodeSnippets;
+  return docsCodeSamples;
 }
