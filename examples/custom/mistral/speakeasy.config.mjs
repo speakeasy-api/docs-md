cd19c27ebfafc30e29dbe9c7bd0c0ee71b214010
nebrius <bryan.hughes@speakeasy.com>
2025-10-02 10:21:15 -0700

    Updated and rebuilt examples

--- examples/custom/mistral/speakeasy.config.mjs
+++ examples/custom/mistral/speakeasy.config.mjs
@@ -47,6 +47,8 @@ export default {
       packageName: "@mistralai/mistralai",
       enableTryItNow: true,
       sdkTarballPath: "../../sdks/mistral-typescript.tar.gz",
+      tryItNowBundlePath: "./public/try-it-now/deps.js",
+      tryItNowBundleUrl: "/try-it-now/deps.js",
     },
     {
       language: "python",
