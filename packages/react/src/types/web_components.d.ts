5412486c3caf5efeda6ab2912c454b0310230cba
nebrius <bryan.hughes@speakeasy.com>
2025-10-09 12:12:41 -0700

    Better types

--- packages/react/src/types/web_components.d.ts
+++ packages/react/src/types/web_components.d.ts
@@ -5,11 +5,19 @@ import type { DetailedHTMLProps, HTMLAttributes } from "react";
 import type { PillElement } from "../components/Pill/Pill.tsx";
 import type { PillProps } from "../components/Pill/types.ts";
 
+type ReactCustomElement<Element extends HTMLElement, Props> = Props &
+  DetailedHTMLProps<HTMLAttributes<Element & Props>, Element>;
+
 declare module "react" {
   namespace JSX {
     interface IntrinsicElements {
-      ["spk-pill"]: PillProps &
-        DetailedHTMLProps<HTMLAttributes<PillElement & PillProps>, PillElement>;
+      ["spk-pill"]: ReactCustomElement<PillElement, PillProps>;
     }
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "spk-pill": PillElement;
   }
 }
