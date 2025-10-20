4fa5f6bfd51d3e5e59df292f0315218183f6f695
nebrius <bryan.hughes@speakeasy.com>
2025-10-20 16:10:27 -0700

    Got vanilla Next.js to work

--- examples/custom/next.config.ts
+++ examples/custom/next.config.ts
@@ -1,4 +1,5 @@
 import createMDX from "@next/mdx";
+import createLit from "@lit-labs/nextjs";
 import remarkGfm from "remark-gfm";
 import remarkHeadingId from "remark-heading-id";
 
@@ -9,10 +10,16 @@ const withMDX = createMDX({
   },
 });
 
-// Merge MDX config with Next.js config
-export default withMDX({
-  // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
-  output: "export",
+const withLitSSR = createLit({
+  webpackModuleRulesTest: /.*\.(js|jsx|ts|tsx|mdx)$/,
 });
+
+// Merge MDX config with Next.js config
+export default withLitSSR(
+  withMDX({
+    // Configure `pageExtensions` to include markdown and MDX files
+    pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+    // Optionally, add any other Next.js config below
+    output: "export",
+  })
+);
--- examples/nextra/next.config.mjs
+++ examples/nextra/next.config.mjs
@@ -1,11 +1,19 @@
 import nextra from "nextra";
+import lit from "@lit-labs/nextjs";
 
 // Set up Nextra with its configuration
 const withNextra = nextra({
   // ... Add Nextra-specific options here
 });
 
-// Export the final Next.js config with Nextra included
-export default withNextra({
-  output: "export",
+const withLitSSR = lit({
+  webpackModuleRulesTest: /.*\.(js|jsx|ts|tsx|mdx)$/,
 });
+
+// Export the final Next.js config with Nextra and Lit SSR included
+export default withNextra(
+  withLitSSR({
+    output: "export",
+    webpackModuleRulesTest: /.*\.(js|jsx|ts|tsx|mdx)$/,
+  })
+);
