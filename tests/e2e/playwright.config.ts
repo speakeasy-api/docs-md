15806d482589f5ba455af539650f36212f9206ca
Jesus Garnica <jesus@speakeasy.com>
2025-10-22 16:13:46 -0700

    consolidate starting and building on CI

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -11,8 +11,6 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -50,20 +48,24 @@ export default defineConfig({
   /* Run dev servers for both example sites before starting tests */
   webServer: [
     {
-      command: "npm run build && npm run start",
+      command: process.env.CI
+        ? "npm run start -- --port 3003"
+        : "npm run build && npm run start -- --port 3003",
       cwd: "../../examples/docusaurus/",
-      url: "http://localhost:3001",
+      url: "http://localhost:3003",
       reuseExistingServer: !process.env.CI,
       stdout: "pipe",
-      timeout: 5 * 60 * 1000, // 5 minutes
+      timeout: 30000 
     },
     {
-      command: "npm run build && npm run start",
+      command: process.env.CI
+        ? "npm run start -- --port 3004"
+        : "npm run build && npm run start -- --port 3004",
       cwd: "../../examples/nextra/",
-      url: "http://localhost:3002",
+      url: "http://localhost:3004",
       reuseExistingServer: !process.env.CI,
       stdout: "pipe",
-      timeout: 5 * 60 * 1000, // 5 minutes
+      timeout: 30000 
     },
   ],
 });
