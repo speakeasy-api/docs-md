d6500ec2a18a99d52859167637a720ef74ed6ce1
Jesus Garnica <jesus@speakeasy.com>
2025-10-22 16:55:47 -0700

    up workers in CI

--- tests/integration/playwright.config.ts
+++ tests/integration/playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */,
   workers: process.env.CI ? 2 : undefined,
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
