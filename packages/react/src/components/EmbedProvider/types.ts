0e867c0beddd524e5bdfedde1c04351450a8ca8f
nebrius <bryan.hughes@speakeasy.com>
2025-10-22 16:37:06 -0700

    Removed first batch of unused overrides

--- packages/react/src/components/EmbedProvider/types.ts
+++
@@ -1,7 +0,0 @@
-import type { FC } from "react";
-
-import type { CloseEmbedIconProps } from "../CloseEmbedIcon/types.ts";
-
-export type EmbedProviderProps = {
-  CloseEmbedIcon?: FC<CloseEmbedIconProps>;
-};
