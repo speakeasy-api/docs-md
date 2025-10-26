14a0290687b89c7da6e55cbd2299d892614661e6
nebrius <bryan.hughes@speakeasy.com>
2025-10-26 11:32:02 -0700

    Added TODO

--- packages/web-components/src/index.ts
+++ packages/web-components/src/index.ts
@@ -3,7 +3,11 @@ import "./types/ambient.ts";
 // Components directly referenced by Markdown
 export { Pill, type PillProps } from "./components/pill/pill.ts";
 
-// Components only referenced by other components
+// TDODO: these components only referenced by other components. We export them
+// while we're in the process of migrating React to Web Components. Once the
+// migration is complete, we should remove them.
+// TODO: we need a new way to include these in ambient declarations too, since
+// we currently crawl this file to generate the ambient declarations.
 export {
   ConnectingCell,
   type ConnectingCellProps,
