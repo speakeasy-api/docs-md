1b8f332807f648919fdce4d8da644a000d308761
nebrius <bryan.hughes@speakeasy.com>
2025-10-27 10:35:32 -0700

    Example fix

--- examples/custom/src/components/Sidebar.tsx
+++ examples/custom/src/components/Sidebar.tsx
@@ -3,8 +3,34 @@
 import Link from "next/link";
 import { useMemo, useState } from "react";
 import styles from "./styles.module.css";
-import { ExpandableCellIcon } from "@speakeasy-api/docs-md-react";
-import path from "path";
+
+function ExpandableCellIcon({
+  className,
+  style,
+}: {
+  className?: string;
+  style?: React.CSSProperties;
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 20"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      width="100%"
+      height="100%"
+      className={className}
+      style={style}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 6.25l-7.5 7.5-7.5-7.5"
+      />
+    </svg>
+  );
+}
 
 type SidebarMetadataItem = {
   slug: string;
